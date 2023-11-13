268ba2c8b67b160752be1cec2e8f1c50b04e1e15
Hernan <hernanparedes55@gmail.com>
2023-11-12 22:51:01 -0300

    visualizacion imagen

--- components/Avatar.js
+++ components/Avatar.js
@@ -5,7 +5,7 @@ const Avatar = () => {
   return (
     <div className='hidden xl:flex xl:max-w-none'>
       <Image
-        src={'/avatarHernan2.png'}
+        src={'/avatarHernan.png'}
         width={737} /*737*/
         height={678} /*678*/
         alt=''
