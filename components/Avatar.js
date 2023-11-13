67f28aeb343c43b9c51bb765c943642ef5a7440d
Hernan <hernanparedes55@gmail.com>
2023-11-12 22:58:50 -0300

    cambio de imagen

--- components/Avatar.js
+++ components/Avatar.js
@@ -5,7 +5,7 @@ const Avatar = () => {
   return (
     <div className='hidden xl:flex xl:max-w-none'>
       <Image
-        src={'/avatarHernan.png'}
+        src={'/avatarHernan2.png'}
         width={737} /*737*/
         height={678} /*678*/
         alt=''
